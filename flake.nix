4864d57beda694bdc9287a2f5980460a1ce5f4f4
wentam <wentam42@gmail.com>
2023-01-05 16:23:43 -0500

    flaxify

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    flake-utils.url = "github:numtide/flake-utils";
+
+    f-alpaca-api-client.url = git+ssh://wentam@wentam.net/mnt/NAS/git-host/alpaca-api-client.git;
+    f-alpaca-api-client.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = {self, nixpkgs, flake-utils, f-alpaca-api-client, ...}:
+  (flake-utils.lib.eachDefaultSystem (system: let pkgs = nixpkgs.legacyPackages.${system}; in rec {
+
+    # Shell
+    devShells.default = pkgs.gcc12Stdenv.mkDerivation {
+      name = "build";
+      buildInputs = with pkgs; [
+        # For mkTestData
+        f-alpaca-api-client.packages.${pkgs.system}.default
+        openssl
+        nlohmann_json
+        libpqxx
+      ];
+    };
+
+    # Package
+    packages.default = pkgs.stdenv.mkDerivation rec {
+      name = "simBroker";
+
+      src = ./.;
+
+      buildInputs = with pkgs; [ gcc12 ];
+
+      dontConfigure = true;
+      makeFlags = [ "-j12" ];
+
+      doCheck = false;
+      checkPhase = ''
+        make test
+      '';
+
+      installPhase = ''
+        mkdir -p $out/lib
+        mkdir -p $out/include
+        cp build/libsimbroker.so $out/lib/libsimbroker.so
+        cp include/simBroker.hpp $out/include/simBroker.hpp
+      '';
+    };
+  }));
+}
